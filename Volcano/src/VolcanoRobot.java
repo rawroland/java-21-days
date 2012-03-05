7715174cb1f88f09cfc4fe4305fb3d3a20e2c074
Roland Awemo <rawroro@googlemail.com>
2012-03-05 11:31:13 +0100

    Volcano helper class

---
+++ Volcano/src/VolcanoRobot.java
@@ -0,0 +1,23 @@
+
+/**
+ * @author roland
+ *
+ */
+public class VolcanoRobot {
+	String status; 
+	int speed;
+	float temperature;
+	
+	void checkTemperature() {
+	  if ( temperature > 660 ) {
+	    status = "returning home";
+	    speed = 5;
+	  }
+	}
+	
+	void showAttributes() {
+	  System.out.println("Status: " + status);
+    System.out.println("Speed: " + speed);
+    System.out.println("Temperature: " + temperature);
+	}
+}
