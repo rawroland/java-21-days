8b7475d76f7499420ddde50ad953b3751545bfc2
Roland Awemo <rawroro@googlemail.com>
2012-05-16 14:54:33 +0200

    Finished

--- Day 17 Networking/src/SuggestsReader.java
+++ Day 17 Networking/src/SuggestsReader.java
@@ -1,12 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class SuggestsReader {
 
   private URL baseURL = null;
-  String relativeSuggests = "autocomplete.php?term=son";
+  String relativeSuggests = "autocomplete.php?term=sams";
   
   public SuggestsReader() {
     try {
@@ -20,17 +26,43 @@ public class SuggestsReader {
    * @param args
    */
   public static void main(String[] args) {
-    
+    System.out.println("Suggestions: " + new SuggestsReader().getSuggestions());
   }
   
   private String getSuggestions() {
+
+    String jsonSuggestions = "";
+    JSONArray suggestionsArray = null;
+    String dataChecker = null;
     try{
       URL suggestsURL = new URL(this.baseURL, this.relativeSuggests);
-      HttpURLConnection connection = (HttpURLConnection) suggestsURL.c
+      HttpURLConnection connection = (HttpURLConnection) suggestsURL.openConnection();
+      System.out.println("URL connection opened.");
+      InputStream is = (InputStream) connection.getContent();
+      InputStreamReader isReader = new InputStreamReader(is, "UTF-8");
+      BufferedReader reader = new BufferedReader(isReader);
+      System.out.println("Getting data...");
+      do {
+        dataChecker = reader.readLine();
+        if ( dataChecker != null) {
+          System.out.println("Step");
+          jsonSuggestions += dataChecker;
+        }
+        System.out.println("Data: " /*+ jsonSuggestions*/ + "::" + dataChecker);
+      } while (dataChecker != null);
     } catch (MalformedURLException malformed) {
       System.out.println("Malformed baseURL: " + malformed.toString());
+    } catch (IOException io) {
+      System.out.println("IO exception: " + io.toString());
     }
-    return null;
+    try {
+      suggestionsArray =new JSONArray(jsonSuggestions);
+    } catch(JSONException jsonexception) {
+      System.out.println("Json exception: " + jsonexception.getMessage());
+    } catch (NullPointerException nullPex) {
+      System.out.println("Null pointer exception: " + nullPex.getMessage());
+    }
+    return jsonSuggestions;
   }
 
 }
